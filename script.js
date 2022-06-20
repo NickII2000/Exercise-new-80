a5bfc7c66106d187267e5112a3539fc54c5a1726
Nick <Nick_II2000@mail.ru>
2022-06-20 07:38:13 +0600

    3-th commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const log = function (a, b, ...rest) {
-    console.log(rest);
+    console.log(a, b, rest);
 };
 
 log(1, 2, 3, 4, 5, 6);
+log('basic', 'add', 'operator', 'usage');
