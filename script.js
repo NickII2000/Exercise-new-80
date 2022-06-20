5ce9adc4f1241ac44439c961061db25779cc06ac
Nick <Nick_II2000@mail.ru>
2022-06-20 07:41:57 +0600

    4-th commit

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ const log = function (a, b, ...rest) {
 
 log(1, 2, 3, 4, 5, 6);
 log('basic', 'add', 'operator', 'usage');
+
+function calcOrDouble(number, basis) {
+    console.log(number * basis);
+}
+
+calcOrDouble(3, 5);
